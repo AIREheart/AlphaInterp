5582d1cd884becaf47c1e4ebe6c0b137b09201bc
Aaisha <103069701+AIREheart@users.noreply.github.com>
2025-11-13 18:18:08 -0500

    Update attention_maps.py

--- visualization/attention_maps.py
+++ visualization/attention_maps.py
@@ -1,3 +1,4 @@
+import py3Dmol
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -8,3 +9,14 @@ def plot_attention(attention_matrix, title="Residue Attention"):
     plt.xlabel("Residue j")
     plt.ylabel("Residue i")
     plt.show()
+
+
+
+def show_structure(pdb_str, residue_importance):
+    view = py3Dmol.view(width=600, height=400)
+    view.addModel(pdb_str, "pdb")
+    for i, score in enumerate(residue_importance):
+        color = f"red" if score > 0.5 else "blue"
+        view.setStyle({'resi': i+1}, {"cartoon": {"color": color}})
+    view.zoomTo()
+    return view
